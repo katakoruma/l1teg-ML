e041e676ad754931084792613552587f3b230dd2
Leon Kerner <leon.kerner@cern.ch>
2025-10-15 18:13:29 +0200

    Add Endcap_rateEff.sh script for cluster pt histogram analysis

---
+++ tasks/EB_TkEle/v0/commands/Endcap_rateEff.sh
@@ -0,0 +1,11 @@
+#Save only the cluster pt histogram
+DIR="/eos/user/l/lekerner/www/l1teg/Endcap/PU200_train"
+#DIR="/eos/user/l/lekerner/l1teg/Endcap/PU200"
+ERA="140Xv0C1"
+
+if [[ "$*" != *"--no_baseline"* ]]; then
+    echo "Running baseline"
+    #run_analysis.py --cfg cfg/l1teg_cfg_endcap.py --mc data/samples.py   --flow flows/Endcap/Baseline/FlowBaseline.py   --plots plots/Endcap/Baseline/PlotEffRate.py  -o $DIR --eras $ERA --noStack
+fi
+
+run_analysis.py --cfg cfg/l1teg_cfg_endcap.py --mc data/TrainTest_samples.py  --flow flows/Endcap/TkEle/FlowBDT_test.py,bdt_path=\"/eos/user/l/lekerner/www/l1teg/Endcap/PU200_train/results/\",dir="float"  --plots plots/Endcap/TkEle/PlotTkEleFeatures.py:score_only=True -o $DIR --eras $ERA --noStack --snapshot --columnSel "TkEle_score,TkEle_(GenEle|CryClu)_pt,TkEle_GenEle_eta,TkEle_(Tk|GenEle)_idx,weight"
