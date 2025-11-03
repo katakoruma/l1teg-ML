97b4c940329955540a801f9931819adb2001f0c8
Leon Kerner <leon.kerner@cern.ch>
2025-11-03 17:09:33 +0100

    model 2

--- models/EB_TkEleID/xgb_2class/v1/params_pu200.py
+++ models/EB_TkEleID/xgb_2class/v1/params_pu200.py
@@ -19,15 +19,15 @@ features = [
     # "CryClu_phi",
 #    "CryClu_showerShape",
     "CryClu_EmIdProb",
-    "CryClu_PuIdProb",
+    # "CryClu_PuIdProb",
     "CryClu_piIdProb",
    # "CryClu_relIso",
 #    "CryClu_caloIso",
 #    "CryClu_standaloneWP",
 #    "CryClu_looseL1TkMatchWP",
     "Tk_chi2RPhi",
-    "Tk_chi2Bend",
-    "Tk_chi2RZ",
+    # "Tk_chi2Bend",
+    # "Tk_chi2RZ",
     "Tk_ptFrac",
     # "Tk_eta",
     # "Tk_phi",
@@ -37,22 +37,22 @@ features = [
    # "GenEle_caloeta",
    # "GenEle_pt",
     "PtRatio",
-    "nTkMatch",
+    # "nTkMatch",
     "absdeta",
-    "absdphi",
-    "CryClu_showerlength",
-    "CryClu_coreshowerlength",
+    # "absdphi",
+    # "CryClu_showerlength",
+    # "CryClu_coreshowerlength",
  #   "CryClu_firstlayer",
  #   "CryClu_first1layers",
  #   "CryClu_first3layers",
     "CryClu_first5layers",
-    "CryClu_firstHcal1layers",
-    "CryClu_firstHcal3layers",
-    "CryClu_firstHcal5layers",
+    # "CryClu_firstHcal1layers",
+    # "CryClu_firstHcal3layers",
+    # "CryClu_firstHcal5layers",
  #   "CryClu_last1layers",
  #   "CryClu_last3layers",
  #   "CryClu_last5layers",
-    "CryClu_emf",
+    # "CryClu_emf",
  #   "CryClu_hoe",
 ]
 
@@ -72,39 +72,39 @@ samples = [
 ]
 
 saturate = {
-#     "CryClu_pt": (0, 1024),
-#     "CryClu_EmIdProb": (0, 1),
-#     "CryClu_PuIdProb": (0, 1),
-#     "CryClu_piIdProb": (0, 1),
-#     "Tk_chi2RPhi": (0, 16),
-#     "Tk_chi2Bend": (0, 16),
-#     "Tk_chi2RZ": (0, 16),
-#     "Tk_ptFrac": (0, 64),
-#  #   "CryClu_relIso": (0, 1),
-#  #   "CryClu_caloIso": (0, 1),
-#  #   "Tk_caloPhi": (0, 64),  # Default range added
-#     "Tk_caloEta": (0, 64),  # Default range added
-#  #    "GenEle_calophi": (0, 64),  # Default range added
-#  #    "GenEle_caloeta": (0, 64),  # Default range added
-#     "GenEle_pt": (0, 64),  # Default range added
-#     "PtRatio": (0, 32),
-#     "nTkMatch": (0, 16),
-#     "absdeta": (0, 8),
-#     "absdphi": (0, 64),
-#     "CryClu_showerlength": (0, 64),
-#     "CryClu_coreshowerlength": (0, 64),
-#     "CryClu_firstlayer": (0, 64),  # Default range added
-#     "CryClu_first1layers": (0, 64),  # Default range added
-#     "CryClu_first3layers": (0, 64),  # Default range added
-#     "CryClu_first5layers": (0, 64),  # Default range added
-#     "CryClu_firstHcal1layers": (0, 64),  # Default range added
-#     "CryClu_firstHcal3layers": (0, 64),  # Default range added
-#     "CryClu_firstHcal5layers": (0, 64),  # Default range added
-#     "CryClu_last1layers": (0, 64),  # Default range added
-#     "CryClu_last3layers": (0, 64),  # Default range added
-#     "CryClu_last5layers": (0, 64),  # Default range added
-# #    "CryClu_emf": (0, 1),
-#     "CryClu_hoe": (0, 64),  # Default range added
+    "CryClu_pt": (0, 128),
+    "CryClu_EmIdProb": (0, 2),
+#    "CryClu_PuIdProb": (0, 2),
+    "CryClu_piIdProb": (0, 2),
+    "Tk_chi2RPhi": (0, 16),
+#    "Tk_chi2Bend": (0, 16),
+#    "Tk_chi2RZ": (0, 16),
+    "Tk_ptFrac": (0, 64),
+#    "CryClu_relIso": (0, 1),
+#    "CryClu_caloIso": (0, 1),
+#    "Tk_caloPhi": (0, 64),  # Default range added
+#    "Tk_caloEta": (0, 64),  # Default range added
+#    "GenEle_calophi": (0, 64),  # Default range added
+#    "GenEle_caloeta": (0, 64),  # Default range added
+#    "GenEle_pt": (0, 64),  # Default range added
+    "PtRatio": (0, 32),
+#    "nTkMatch": (0, 16),
+    "absdeta": (0, 8),
+#    "absdphi": (0, 64),
+#    "CryClu_showerlength": (0, 64),
+#    "CryClu_coreshowerlength": (0, 64),
+#    "CryClu_firstlayer": (0, 64),  # Default range added
+#    "CryClu_first1layers": (0, 64),  # Default range added
+#    "CryClu_first3layers": (0, 64),  # Default range added
+    "CryClu_first5layers": (0, 1),  # Default range added
+#    "CryClu_firstHcal1layers": (0, 64),  # Default range added
+#    "CryClu_firstHcal3layers": (0, 64),  # Default range added
+#    "CryClu_firstHcal5layers": (0, 64),  # Default range added
+#    "CryClu_last1layers": (0, 64),  # Default range added
+#    "CryClu_last3layers": (0, 64),  # Default range added
+#    "CryClu_last5layers": (0, 64),  # Default range added
+#    "CryClu_emf": (0, 1),
+#    "CryClu_hoe": (0, 64),  # Default range added
 }
 
 features = [f"TkEle_{f}" for f in features]
